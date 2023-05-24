8748f229401cc932a762e6136ab45c327f619fef
Sid Shingaki <sidalaytv@gmail.com>
2023-05-24 03:09:59 -0700

    feat: ExplosiveBarrel

---
+++ Source/ActionRoguelike/Public/ExplosiveBarrel.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ExplosiveBarrel.generated.h"
+
+class URadialForceComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API AExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AExplosiveBarrel();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	URadialForceComponent* RadialForceComp;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
